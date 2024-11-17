64eb35e9db5688649eb3d8fabac86d775d695547
ann360 <82609121+ann360@users.noreply.github.com>
2024-11-18 03:07:22 +0400

    Add files via upload

---
+++ cv-html/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+$('#menu').click(function (){
+ $(this).toggleclass('fa-times');
+
+   $('header').toggleClass('toggle');
+});
+
+$(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+
+   $('header').removeClass('toggle');
+
+
+
+});
+
+
+});
+
